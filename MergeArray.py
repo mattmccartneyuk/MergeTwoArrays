ecae4cc7a56620fcaea5bcb4400d6c050d5155a0
Matthew McCartney <matt.mccartney@live.co.uk>
2022-11-26 18:55:25 +0000

    Added comments to occurrence checker.

--- MergeArray.py
+++ MergeArray.py
@@ -1,18 +1,37 @@
 def main(array1, array2):
     occurrence_tracker = {}
+    
+    """
+    *******************************************************
+    ** The first For Loop adds the occurrences of {array1}
+    ** to {occurrence_tracker}, followed by {array2}. This
+    ** loop only operates twice.
+    *******************************************************
+
+    """
     current_array = array1
-    for y in range(2): 
+    for y in range(2):
+        """
+        *******************************************************
+        ** The second For Loop checks if {current_array} already
+        ** exists in the occurence tracker. If it doesn't it
+        ** creates a new key/value pair. If the key/value pair
+        ** does exist the key's value is incremented by +1.
+        *******************************************************
+        """
         for i in range(len(current_array)):
-            if str(current_array[i]) in occurrence_tracker:
-                number = current_array[i]
-                #Get current occurrence, then + 1
-                new_occurrence = occurrence_tracker.get(f"{number}")
+            current_number = current_array[i]
+            # Checks if {current_number} already exists as a key in {occurrence_tracker}.
+            if str(current_number) in occurrence_tracker:
+                # .get returns the current value using the key.
+                new_occurrence = occurrence_tracker.get(f"{current_number}")
+                # Increments value by +1. 
                 new_occurrence = str(int(new_occurrence) + 1)
-                print(new_occurrence)
-                occurrence_tracker.update({f"{number}": f"{new_occurrence}"})
+                # .update changes the value held in {occurrence_tracker} using the key.
+                occurrence_tracker.update({f"{current_number}": f"{new_occurrence}"})
             else:
-                number = current_array[i]
-                occurrence_tracker[f"{number}"] = "1"
+                # Creates a new key/value pair in the occurrence tracker where {current_number} is the current_array value being considered.
+                occurrence_tracker[f"{current_number}"] = "1"
         current_array = array2
 
     mergeList = []
@@ -25,10 +44,10 @@ def main(array1, array2):
 
         mergeList.extend(string.split(','))
     
-    print(mergeList)
+    return mergeList
 
 if __name__ == "__main__":
     a1 = [1, 1, 2, 2, 3]
     a2 = [1, 2, 3, 4, 5, 6] 
-    # return 1, 1, 1, 2, 2, 2, 3, 3, 4, 5, 6
-    main(a1, a2)
+   
+    print(main(a1, a2))
